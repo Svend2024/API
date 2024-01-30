0b9a03c9090126b9da3a48eccfa6eaf4f4ab0105
LucasLadjevardi <lucladjevardi@gmail.com>
2024-01-30 21:12:06 +0100

    push 3

--- KameGameAPI/obj/Debug/net7.0/KameGameAPI.AssemblyInfo.cs
+++ KameGameAPI/obj/Debug/net7.0/KameGameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KameGameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03878cba6670e8b22254be321329ba323e67fec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KameGameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KameGameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
